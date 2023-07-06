7f09af05aa886efd5c67332cecc07023c94d6777
CryF1sh <glebbogomolov04@gmail.com>
2023-07-06 11:23:46 +0300

    Выполнил задание, но при вводе 2 символов не работает

--- MaximTechnologyPractice/Program.cs
+++ MaximTechnologyPractice/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using System.Text;
+
+Console.WriteLine("Введите строку:");
+string inputStr = Console.ReadLine();
+if (string.IsNullOrEmpty(inputStr))
+{
+    Console.WriteLine("Строка не должна быть пустой!");
+    return;
+}
+string outputStr = StringProcessing(inputStr);
+Console.WriteLine("Обработанная строка:");
+Console.WriteLine(outputStr);
+Console.ReadKey();
+
+
+// Метод обработки строки из первого задания
+static string StringProcessing(string inputStr)
+{
+    StringBuilder resultStr = new StringBuilder();
+    if (inputStr.Length % 2 == 0)
+    {
+        //Если строка чётная
+        int halfLength = inputStr.Length / 2;
+        string firstHalf = inputStr.Substring(0, halfLength);
+        string secondHalf = inputStr.Substring(halfLength);
+
+        resultStr.Append(string.Join("", firstHalf.Reverse()));
+        resultStr.Append(string.Join("", secondHalf.Reverse()));
+    }
+    else
+    {
+        //Если строка нечётная 
+        resultStr.Append(string.Join("", inputStr.Reverse()));
+        resultStr.Append(inputStr);
+    }
+    return resultStr.ToString();
+}
